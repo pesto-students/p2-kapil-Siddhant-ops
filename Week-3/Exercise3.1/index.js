471d50b133aa4e2313846eb320d3374ea2b70131
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-06-02 09:46:10 +0530

    W3 Exercise 3.1 fixed for better optimization

--- Week-3/Exercise3.1/index.js
+++ Week-3/Exercise3.1/index.js
@@ -1,7 +1,7 @@
 const memoize = (func) => {
   const cache = new Map();
   return (...args) => {
-    let strKey = args.sort().join(",");
+    let strKey = args.join(",");
     if (!cache.has(strKey)) {
       cache.set(strKey, func.apply(this, args));
       return func.apply(this, args);
@@ -17,18 +17,15 @@ const add = (...args) => {
 };
 
 const memoizeAdd = memoize(add);
-console.time();
-memoizeAdd(100, 100); // returns 200, since the key i.e "100,100" is not already in cache it's computed & added in
-console.timeEnd();
-console.time();
-memoizeAdd(100); // returns 100, since the key i.e "100" is not already in cache it's computed & added in
-console.timeEnd();
-console.time();
-memoizeAdd(100, 200); // returns 300, since the key i.e "100,200" is not already in cache it's computed & added in
-console.timeEnd();
-console.time();
-memoizeAdd(100, 100); // returns 200, since the key i.e "100,100" is in cache the value is returned directly instead of computing it again
-console.timeEnd();
-console.time();
-memoizeAdd(200, 100); // returns 300, since the key i.e "100,200" is in cache the value is returned directly instead of computing it again
-console.timeEnd();
+
+const debugMemoiz = (...args) => {
+  console.time();
+  memoizeAdd(...args);
+  console.timeEnd();
+};
+
+debugMemoiz(100, 100); // returns 200, since the key i.e "100,100" is not already in cache it's computed & added in
+debugMemoiz(100); // returns 100, since the key i.e "100" is not already in cache it's computed & added in
+debugMemoiz(100, 200); // returns 300, since the key i.e "100,200" is not already in cache it's computed & added in
+debugMemoiz(100, 100); // returns 200, since the key i.e "100,100" is in cache the value is returned directly instead of computing it again
+debugMemoiz(200, 100); // returns 300, since the key i.e "100,200" is in cache the value is returned directly instead of computing it again
