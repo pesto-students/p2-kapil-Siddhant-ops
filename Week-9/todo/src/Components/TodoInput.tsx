5d9c52fc4cb4a5a525d88872f3e22b3fbbbf0c31
Siddhant-ops <siddhantdalvi3@gmail.com>
2022-07-17 12:53:15 +0530

    W9 creating TodoInput HTML structure

--- Week-9/todo/src/Components/TodoInput.tsx
+++ Week-9/todo/src/Components/TodoInput.tsx
@@ -1,5 +1,14 @@
+import inputStyles from "../Styles/input.module.css";
+
 const TodoInput = () => {
-  return TodoInput;
+  return (
+    <section className={inputStyles.inputContainer}>
+      <form>
+        <input type="text" name="input" placeholder="What needs to be done?" />
+        <button type="submit">Add task</button>
+      </form>
+    </section>
+  );
 };
 
 export default TodoInput;
